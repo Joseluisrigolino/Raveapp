9330ffebc72adaf7ea1dc35f2a93c2b96fb6d1c8
Jose Luis Rigolino <rigolinojoseluis@gmail.com>
2025-02-24 21:47:00 -0300

    Arreglo de card en menu

--- components/CardComponent.tsx
+++ components/CardComponent.tsx
@@ -4,41 +4,26 @@ import { Card, Paragraph, TouchableRipple } from "react-native-paper";
 import TitlePers from "./TitleComponent";
 
 const CardComponent = ({ title = "", text = "", foto = "", onPress }) => (
-  <View style={styles.container}>
-    <TouchableRipple onPress={() => onPress(title)} borderless>
-      <Card style={styles.card}>
-        <Card.Content style={styles.cardContent}>
-          <TitlePers text={title} />
-          <Paragraph>{text}</Paragraph>
-        </Card.Content>
-        {/* Aseguramos que la imagen ocupe el ancho completo */}
-        <Card.Cover source={{ uri: foto }} style={styles.cardCover} />
-      </Card>
-    </TouchableRipple>
-  </View>
+  <TouchableRipple onPress={() => onPress(title)} borderless>
+    <Card style={styles.card}>
+      <Card.Content>
+        <TitlePers text={title} />
+        <Paragraph>{text}</Paragraph>
+      </Card.Content>
+      <Card.Cover source={{ uri: foto }} style={styles.cardCover} />
+    </Card>
+  </TouchableRipple>
 );
 
 const styles = StyleSheet.create({
-  container: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-  },
   card: {
-    marginTop: 10,
-    marginBottom: 15,
-    width: "100%", // Asegura que la tarjeta ocupe todo el ancho disponible
-  },
-  cardContent: {
-    fontWeight: "bold",
-    marginTop: 10,
-    marginBottom: 15,
-    width: "100%",
+    marginVertical: 10,
+    width: "100%", // Ocupa todo el ancho disponible
   },
   cardCover: {
-    width: "100%", // Hace que la imagen ocupe todo el ancho de la tarjeta
-    height: 200, // Ajusta la altura a tu preferencia
-    resizeMode: "cover", // Asegura que la imagen se expanda correctamente
+    width: "100%", // Imagen ocupa todo el ancho de la tarjeta
+    height: 200,
+    resizeMode: "cover",
   },
 });
 
